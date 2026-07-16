95bea54534491be76bda5b03a0b2cf52f4d76467
Arakai <1099417497@qq.com>
2026-07-16 17:57:20 +0800

    @ fix：Creem checkout尝试关闭testMode @

--- app/api/creem/checkout/route.ts
+++ app/api/creem/checkout/route.ts
@@ -2,6 +2,6 @@ import { Checkout } from "@creem_io/nextjs";
 
 export const GET = Checkout({
   apiKey: process.env.CREEM_API_KEY!,
-  testMode: true,
+  testMode: false,
   defaultSuccessUrl: `${process.env.NEXT_PUBLIC_APP_URL}/en/my?payment=success`,
 });
