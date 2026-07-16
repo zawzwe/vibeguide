bfada6d7ce0a999ea1b10a207aeb0fe1346e6529
Arakai <1099417497@qq.com>
2026-07-16 17:55:25 +0800

    @ fix：Creem checkout路由改为GET方法
    SDK的Checkout通过query参数+浏览器导航工作，需export GET而非POST
    @

--- app/api/creem/checkout/route.ts
+++ app/api/creem/checkout/route.ts
@@ -1,6 +1,6 @@
 import { Checkout } from "@creem_io/nextjs";
 
-export const POST = Checkout({
+export const GET = Checkout({
   apiKey: process.env.CREEM_API_KEY!,
   testMode: true,
   defaultSuccessUrl: `${process.env.NEXT_PUBLIC_APP_URL}/en/my?payment=success`,
