f1046d6d2dcee477dfc051f8636b70cd9bce2b3f
Arakai <1099417497@qq.com>
2026-07-21 14:41:55 +0800

    @ feat：接入TikTok Pixel追踪代码
    在根布局加入TikTok Pixel，用于广告转化追踪
    @

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -6,6 +6,7 @@ import { notFound } from "next/navigation";
 import { ThemeProvider } from "next-themes";
 import { routing } from "@/i18n/routing";
 import { Analytics } from "@vercel/analytics/next";
+import Script from "next/script";
 import "../globals.css";
 
 const defaultUrl = process.env.VERCEL_URL
@@ -59,6 +60,15 @@ export default async function LocaleLayout({
   return (
     <html lang={locale} suppressHydrationWarning>
       <body className={`${geistSans.className} antialiased`}>
+        <Script
+          id="tiktok-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+!function(w,d,t){w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e};ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script");n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};ttq.load('D9C69FJC77UFCF7AQL6G');ttq.page();}(window,document,'ttq');
+`,
+          }}
+        />
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
